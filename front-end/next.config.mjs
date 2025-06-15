424df840440bc315c88f9e64cf69d0c5051242c9
Gabriel <gabrieloliveira75xx@gmail.com>
2025-06-15 13:01:26 -0300

    adicionado servidor nginx para servir conteúdos

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,26 +1,68 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Desabilita o ESLint no processo de build
-      ignoreDuringBuilds: true,
-    },
-    webpack(config, { isServer }) {
-      // Ignora todos os warnings durante o build
-      config.ignoreWarnings = [
-        (warning) => true, // Isso ignora todos os warnings
-      ];
-      return config;
-    },
-  };
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    // Desabilita o ESLint no processo de build
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignora todos os erros de tipo durante o build
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      'grupoglk.com.br',
+    ],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+  },
+  experimental: {
+    optimizeCss: false,
+    serverExternalPackages: [],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
+    ]
+  }
+}
 
-export default {
-    typescript: {
-      // Ignora todos os erros de tipo durante o build
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      // Ignora todos os erros de linting durante o build
-      ignoreDuringBuilds: true,
-    },
-  };
+export default nextConfig
   
